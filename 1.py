1c59337a761cfe06484aa0b84e1d0dbb5c986eff
moynul457 <69568834+moynul457@users.noreply.github.com>
2021-08-03 22:30:20 +0600

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.speed(0)
+turtle.bgcolor("black")
+for i in range (91) :
+    for colours in ("red", "magenta", "blue", "cyan", "green", "yellow", "white") :
+        turtle.color(colours)
+        turtle.pensize(4)
+        turtle.left(0)
+        turtle.forward(200)
+        turtle.left(120)
+        turtle.forward(200)
+        turtle.left(120)
+        turtle.forward(200)
+        turtle.left(123)
